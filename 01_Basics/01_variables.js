112b1ff4e72dbd8e5efec0cab5b1a4afc4c4a950
Ayush-kush <113274708+Ayush-kush@users.noreply.github.com>
2024-02-24 12:03:13 +0000

    2 Lectures Done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+console.log('hello')
+
+const username = 'Ayush Kushwah';
+
+// username = 'Thanos'; cant reassign a value to const variable
+
+let accountEmail = "Ak@ak.com";
+var accountPass= "10234";
+accountCity="Gwalior";
+
+console.table([username,accountEmail,accountPass,accountCity]);
