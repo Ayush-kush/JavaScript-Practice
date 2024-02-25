1e6de3bd68ec918d464c67ddd13c70e62135331d
Ayush-kush <ayushkushwah678@gmail.com>
2024-02-25 15:47:17 +0530

    Type Conversion Learned

---
+++ 01_Basics/03_dataConversion.js
@@ -0,0 +1,42 @@
+
+// NUM TO STRING
+// let marks = 100
+
+// console.log(marks);
+// console.log(typeof marks);
+
+// let newMarks = String(marks);
+
+// console.log(newMarks);
+// console.log(typeof newMarks);
+
+
+// STRING TO NUM
+// let phoneNumber= "108"
+// console.log(phoneNumber);
+// console.log(typeof phoneNumber);
+
+// let phNum = Number(phoneNumber)
+// console.log(phNum, typeof phNum);
+
+//WHAT IF STRING CONTAIN LETTERS BEFORE TYPE CONVERSION
+// let phoneNumber= "108Acb"
+// console.log(phoneNumber);
+// console.log(typeof phoneNumber);
+
+// let phNum = Number(phoneNumber)
+// console.log(phNum, typeof phNum); //gives NAN on output but Typeof NAN is Number XD...
+
+// let truth = 1
+// let lie = 0
+// console.log(Boolean(truth),Boolean(lie));
+
+// let sach = true
+// let juth = false
+// console.log(Number(sach),Number(juth));
+// console.log(String(sach),String(juth));
+
+// let loggedIn = ""
+// console.log(Boolean(loggedIn),typeof Boolean(loggedIn)); // Empty string gives false after conversion to bool
+// Else it gives true for any string
+
