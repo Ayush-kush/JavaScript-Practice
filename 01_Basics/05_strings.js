9a50d9ff258ac914b7a7b4e79ba6fe8c300381b0
Ayush-kush <ayushkushwah678@gmail.com>
2024-02-27 05:03:38 +0530

    Strings done

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,29 @@
+// let gameName = 'wraith' //STRING
+// let game = new String("Ayush") //STRING OBJECT
+// console.log(gameName);
+// console.log(game);
+// console.log(typeof(gameName),typeof game);
+
+// console.log(gameName+game+" Okie pokie"); //OLD WAY TO CONCATENATE STRING
+// let sentence = `This is my javascript practice series for web devlopment and i am ${gameName}`; //PREFER TO USE THIS
+// console.log(sentence);
+
+// let str="      Ayush-Kushwah           "
+
+// console.log(str);
+// console.log(str.trim()); //REMOVE WHITESPACES
+
+let str2="Ayush-Kushwah"
+
+console.log(str2.charAt(3));
+console.log(str2.endsWith('h'));
+console.log(str2.length);
+console.log(str2.split('-'));
+console.log(str2.replace('-',"op"));
+
+
+
+
+
+
+
