2181894e7e28b56816585e4e40dbf9719260f8b4
Ayush-kush <ayushkushwah678@gmail.com>
2024-02-28 21:17:56 +0530

    array basics part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,35 @@
+// const heroes = ["Iron Man", "Hulk", "Thor", 10, 10.2];
+// console.log(heroes);
+
+const myArr = new Array(10,20,30,40,50);
+// console.log(myArr);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) -> Inserts an element from left
+// console.log(myArr);
+
+// myArr.shift() -> Removes an element from left
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join('-')
+
+// console.log(myArr);
+// console.log( newArr);
+
+// console.log(myArr);
+// console.log(myArr.slice(1,3)); //   -> slice(i,j) returns an copy of array by slicing from index i to j-1
+// console.log(myArr);
+// console.log(myArr.splice(1,3)); // -> splice(i,j) returns an subarray from org array from index i to j and changes org array also
+// console.log(myArr);
+
+
+
+
