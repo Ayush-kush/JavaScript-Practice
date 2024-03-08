95df0d128169a5de03327bf7786392b76cb6033d
Ayush-kush <ayushkushwah678@gmail.com>
2024-03-08 17:07:07 +0530

    IIFE

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -58,3 +58,14 @@ chai()
 // }
 
 const addTwo = (num1, num2) =>  num1 + num2
+
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
